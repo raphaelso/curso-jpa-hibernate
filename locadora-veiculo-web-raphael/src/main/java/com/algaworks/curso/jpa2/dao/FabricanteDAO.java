f2a7395c0a5917c87705f53cd36950817e172712
Raphael Oliveira <raphaelsoliveira@gmail.com>
2016-02-19 17:01:14 -0200

    alterações

---
+++ locadora-veiculo-web-raphael/src/main/java/com/algaworks/curso/jpa2/dao/FabricanteDAO.java
@@ -0,0 +1,30 @@
+package com.algaworks.curso.jpa2.dao;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+
+import com.algaworks.curso.jpa2.modelo.Fabricante;
+import com.algaworks.curso.jpa2.service.NegocioException;
+
+public class FabricanteDAO implements Serializable {
+
+	@Inject
+	private EntityManager em;
+	
+	public void salvar(Fabricante fabricante) {
+		em.persist(fabricante);
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<Fabricante> buscarTodos() {
+		return em.createQuery("from Fabricante").getResultList();
+	}
+
+	public void excluir(Fabricante fabricanteSelecionado) throws NegocioException {
+		
+	}
+	
+}
